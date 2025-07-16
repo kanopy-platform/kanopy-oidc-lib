1d253272e94839332816325c267fc96f7158608c
Yuzhou Liu <yuzhou.liu@mongodb.com>
2025-07-16 15:48:24 -0400

    removed unused licensed.go reviewed, changing example

--- examples/main.go
+++ examples/main.go
@@ -2,27 +2,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/kanopy-platform/kanopy-oidc-lib/pkg/dex"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
 
 	demoURL := "https://dex.example.com"
 
-	client, err := dex.NewClient(dex.WithConnectorID("mockcallback"), dex.WithIssuer(demoURL), dex.WithClientID("mocksecure"), dex.WithSecret("hijklmnop"))
+	// The example below won't work, requires real values
+	token, err := dex.NewClientGetToken(
+		dex.WithConnectorID("mockcallback"),
+		dex.WithIssuer(demoURL),
+		dex.WithClientID("mocksecure"),
+		dex.WithSecret("hijklmnop"),
+		dex.WithContext(ctx),
+	)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer client.Close()
 
-	tok, err := client.Token()
-	if err != nil {
-		fmt.Println(err)
-		return
+	if token != nil {
+		// There are context cancelled scenarios where the token may be nil.
+		fmt.Println(token.AccessToken)
 	}
-
-	fmt.Println(tok)
 }
